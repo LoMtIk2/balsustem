e70ada49d2264a3fada027c24d3d76c0dd00106d
LoMtIk2 <72619523+LoMtIk2@users.noreply.github.com>
2024-11-09 03:03:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -48,12 +48,39 @@ addButton.onclick = function () {
         };
 
         const deleteButton = document.createElement('button');
-        deleteButton.innerText = '';
         deleteButton.className = 'delete-button';
         deleteButton.onclick = function () {
             blocksContainer.removeChild(block);
             toggleSortButton();
         };
+        
+        // Создаем SVG элемент
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        svg.setAttribute("width", "16");
+        svg.setAttribute("height", "16");
+        svg.setAttribute("fill", "currentColor");
+        svg.setAttribute("class", "bi bi-trash");
+        svg.setAttribute("viewBox", "0 0 16 16");
+        
+        // Создаем первый путь
+        const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path1.setAttribute("d", "M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z");
+        
+        // Создаем второй путь
+        const path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path2.setAttribute("fill-rule", "evenodd");
+        path2.setAttribute("d", "M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z");
+        
+        // Добавляем пути в SVG
+        svg.appendChild(path1);
+        svg.appendChild(path2);
+        
+        // Добавляем SVG в кнопку
+        deleteButton.appendChild(svg);
+        
+        // Теперь deleteButton можно добавить в контейнер или на страницу
+        
 
         buttonsDiv.appendChild(plusButton);
         buttonsDiv.appendChild(minusButton);
